04ddf94902f95a19e99718272198fdb4133b4691
kushh304 <65391920+kushh304@users.noreply.github.com>
2022-03-29 14:21:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ import requests
 import config
 import pickle
 import io
-import torch
-from torchvision import transforms
 from PIL import Image
 from utils.model import ResNet9
 # ==============================================================================================
